6ede3a2c346623536990b55a097e2af4c401068f
jwmuller <jwmuller@umich.edu>
2022-03-11 16:53:18 -0500

    driver

--- main.cpp
+++ main.cpp
@@ -9,6 +9,22 @@
 
 int main(int argc, char **argv)
 {
+  // prompts user for what gamemode they want (helper or solver)
+
+  // prompts user to pick what list they are playing with (from given choices)
+  
+  // prompts user for initial guess or gives user best guess
+
+
+  // prompts user for intial guess results
+
+  // loop starts here  
+
+  // outputs suggested guess
+  // this suggested guess is based on the narrowed down list from the previous guess' result
+
+  // prompts user for suggested guess result
+
   printf("Hello World!");
   return 0;
 }

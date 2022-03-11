cea935fbc60352cd4f3795813b11a167130aeaac
azhangerson <101429875+azhangerson@users.noreply.github.com>
2022-03-11 16:29:13 -0500

    Update wordleFunctions.h

--- wordleFunctions.h
+++ wordleFunctions.h
@@ -1,10 +1,47 @@
 // Outlines the functions used for processing
+ #include <cmath>
+ #include <iostream>
+ #include <fstream>
+ #include <algorithm>
+ #include <vector>
+ #include <string>
+
+using namespace std;
+
+struct word
+{
+    string wd;
+    int freq[26];
+};
+
+struct rightPos
+{
+    char letter;
+    int pos;
+};
+
+struct wrongPos
+{
+    char letter;
+    int wrongPos[5];
+};
 
 class Wordle
 {
   private:
-  
+    vector<word> wordList;
+    string guess;
+    vector<rightPos> rightLetters;
+    vector<wrongPos> wrongPosLetters;
+    vector<char> wrongLetters;
+    vector<string> guesses;
+    
   public:
     void errorCheck();
     
-}
+    void readList();
+
+    void readFreq();
+
+    void readPos();
+};

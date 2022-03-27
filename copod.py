e7ea52ef869685b8a5e35ba13c3e83051f9652a3
jwmuller <jwmuller@umich.edu>
2022-03-27 17:39:22 -0400

    python code created

--- copod.py
+++ copod.py
@@ -1,1 +1,21 @@
+# Implementation of COPOD Machine Learning Algorithm
+
+# Read in word list
+
+# Frequency Analysis on Word list
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
+msg = 'hello'
+print(msg)
+# COPOD algorithm
+
+
+
+
+
 

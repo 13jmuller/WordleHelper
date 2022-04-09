cb5b7417bafae7d9bdb3f2b25e903ccf8aee90fd
Justin Yu <yujustin8@gmail.com>
2022-04-08 23:07:54 -0700

    HeuristicRanker Completed

--- GreedyHeuristic.py
+++ GreedyHeuristic.py
@@ -12,6 +12,7 @@ class Greedy(object):
         self.guess = ""
         self.stage = 1
         self.gamelist = self.get_list(WORDSFILE)
+        self.wordf = self.get_list(WORDSFILE)
         self.previouslistlen = 0
         self.list_size = 0
         self.exhausted_letters = []
@@ -75,13 +76,38 @@ class Greedy(object):
     '''
     process
     '''
-    
+    def generate_square_perms(self):
+        sq = ["g", "y", "-"]
+        sqperms = []
+        perm = "" 
+        for a in sq:
+            for b in sq:
+                for c in sq:
+                    for d in sq:
+                        for e in sq:
+                            perm = a+b+c+d+e
+                            sqperms.append(perm)        
+        sqperms.remove('ggggg')
+        sqperms.remove('ggggy')
+        sqperms.remove('gggyg')
+        sqperms.remove('ggygg')
+        sqperms.remove('gyggg')
+        sqperms.remove('ygggg')
+        return sqperms
+
+    def reset_game(self):
+        self.squares = ["-","-","-","-","-"]
+        self.guess = ""
+        self.stage = 1
+        self.gamelist = self.wordf
+        self.previouslistlen = 0
+        self.list_size = 0
+        self.exhausted_letters = []
+
     def remaining_words(self):
         #returns a list of strings - remaining guessable words
         rem = self.gamelist
-        print(rem)
         self.previouslistlen = len(rem)
-        print(self.previouslistlen)
         rem.remove(self.guess)
         for word in rem[:]:
             remove = False
@@ -103,9 +129,40 @@ class Greedy(object):
         self.gamelist = rem
         self.stage += 1
         return rem
-    
+
+    def remaining_words_lite(self):
+        #returns number of remaining guessable words
+        ct = 1
+        self.gamelist.remove(self.guess)
+        for word in self.gamelist:
+            remove = False
+            word_letters = split_str_to_char(word)
+            for i in range(5):
+                if(self.squares[i] == "g" and word_letters[i]!=split_str_to_char(self.guess)[i]):
+                    remove=True
+                elif(self.squares[i] == "y" and split_str_to_char(self.guess)[i] not in word_letters):
+                    remove=True
+                elif(self.squares[i] == "y" and word_letters[i]==split_str_to_char(self.guess)[i]):
+                    remove=True
+                elif(self.squares[i] == "-" and split_str_to_char(self.guess)[i] not in self.exhausted_letters):
+                    self.exhausted_letters.append(split_str_to_char(self.guess)[i])
+                elif(self.squares[i] == "-" and split_str_to_char(self.guess)[i] in word_letters):
+                    remove=True
+                elif(word_letters[i] in self.exhausted_letters):
+                    remove=True
+            if(remove): ct += 1
+        return 12966-ct
+
     def expectedinfo(self):
         p = len(self.gamelist)/self.previouslistlen
+        if p==0:
+            return 0
+        return p*math.log(1/p,2)
+
+    def expectedinfo_lite(self, n):
+        p = n/12966
+        if p==0:
+            return 0
         return p*math.log(1/p,2)
 
     """
@@ -132,12 +189,31 @@ class Greedy(object):
                 print("Expected Information Value: ")
                 print(self.expectedinfo())
     
-    def sim():
-        return False
+    def generate_greedy_ranks(self):
+        sqperms = self.generate_square_perms()
+        ranks = []
+        ct = 1
+        for word in self.gamelist[:]:
+            E = 0
+            for perm in sqperms:
+                self.guess = word
+                self.squares = perm
+                n = self.remaining_words_lite()
+                E += self.expectedinfo_lite(n)
+                self.reset_game()
+            print(ct)
+            print(word)
+            print(E)
+            ct += 1
+            ranks.append([word, E])
+
+        with open('GreedyHeuristicRank.txt', 'w') as f:
+            for item in ranks:
+                f.writelines(item[0] + ", " + str(item[1]) + "\n")
 
         
 if __name__ == "__main__":
     greedygame = Greedy(WORDSFILE = "wordleFullList.txt", Verbose=True)
-    greedygame.wordle_helper()
+    greedygame.generate_greedy_ranks()
 
 
